Jookia/u-boot

commit 9783f7d28da4d7201a613dc1606876415c49aaf2

sunxi: Support UART2 on the T113
The T113 supports UART2 on PD1 and PD2. Add it as an option.

Signed-off-by: John Watts <contact@jookia.org>

arch/arm/mach-sunxi/board.c
@@ -166,6 +166,10 @@ static int gpio_init(void)
 	sunxi_gpio_set_cfgpin(SUNXI_GPB(0), SUN8I_GPB_UART2);
 	sunxi_gpio_set_cfgpin(SUNXI_GPB(1), SUN8I_GPB_UART2);
 	sunxi_gpio_set_pull(SUNXI_GPB(1), SUNXI_GPIO_PULL_UP);
+#elif CONFIG_CONS_INDEX == 3 && defined(CONFIG_MACH_SUN8I_R528)
+	sunxi_gpio_set_cfgpin(SUNXI_GPD(1), SUN8I_T113_GPD_UART2);
+	sunxi_gpio_set_cfgpin(SUNXI_GPD(2), SUN8I_T113_GPD_UART2);
+	sunxi_gpio_set_pull(SUNXI_GPD(2), SUNXI_GPIO_PULL_UP);
 #elif CONFIG_CONS_INDEX == 4 && defined(CONFIG_MACH_SUN8I_R528)
 	sunxi_gpio_set_cfgpin(SUNXI_GPB(6), 7);
 	sunxi_gpio_set_cfgpin(SUNXI_GPB(7), 7);

include/sunxi_gpio.h
@@ -120,6 +120,7 @@ enum sunxi_gpio_number {
 #define SUN8I_A83T_GPB_UART0	2
 #define SUN8I_V3S_GPB_UART0	3
 #define SUN50I_GPB_UART0	4
+#define SUN8I_T113_GPD_UART2	5
 
 #define SUNXI_GPC_NAND		2
 #define SUNXI_GPC_SPI0		3

commit d37ec8a4d6468a41d306f670477045e8c22d8b00

sunxi: Support UART2 on PD1/PD2 on the R528, D1 and T113
These chips support UART2 on the PD1/PD2 pins. Add a separate config
option and code paths to use these pins.

Fixes: 9783f7d28da4d7201a613dc1606876415c49aaf

arch/arm/mach-sunxi/Kconfig
@@ -748,6 +748,13 @@ config UART0_PORT_F
 	at the same time, the system can be only booted in the FEL mode.
 	Only enable this if you really know what you are doing.
 
+config UART2_PORT_D
+	bool "UART2 on PD1 and PD2"
+	depends on MACH_SUN8I_R528
+	---help---
+	Use PD1 and PD2 for UART2 on the R528, D1 and T113 chips.
+	Enable this if you need UART2 on these pins.
+
 config OLD_SUNXI_KERNEL_COMPAT
 	bool "Enable workarounds for booting old kernels"
 	---help---

arch/arm/mach-sunxi/board.c
@@ -166,6 +166,10 @@ static int gpio_init(void)
 	sunxi_gpio_set_cfgpin(SUNXI_GPB(0), SUN8I_GPB_UART2);
 	sunxi_gpio_set_cfgpin(SUNXI_GPB(1), SUN8I_GPB_UART2);
 	sunxi_gpio_set_pull(SUNXI_GPB(1), SUNXI_GPIO_PULL_UP);
+#elif CONFIG_CONS_INDEX == 3 && defined(CONFIG_MACH_SUN8I_R528) && defined(CONFIG_UART2_PORT_D)
+	sunxi_gpio_set_cfgpin(SUNXI_GPD(1), SUN8I_R528_GPD_UART2);
+	sunxi_gpio_set_cfgpin(SUNXI_GPD(2), SUN8I_R528_GPD_UART2);
+	sunxi_gpio_set_pull(SUNXI_GPB(2), SUNXI_GPIO_PULL_UP);
 #elif CONFIG_CONS_INDEX == 3 && defined(CONFIG_MACH_SUN8I_R528)
 	sunxi_gpio_set_cfgpin(SUNXI_GPB(0), SUN8I_R528_GPB_UART2);
 	sunxi_gpio_set_cfgpin(SUNXI_GPB(1), SUN8I_R528_GPB_UART2);

drivers/pinctrl/sunxi/pinctrl-sunxi.c
@@ -613,7 +613,11 @@ static const struct sunxi_pinctrl_function sun20i_d1_pinctrl_functions[] = {
 	{ "uart0",	6 },	/* PB0-PB1, PB8-PB9, PE2-PE3 */
 #endif
 	{ "uart1",	2 },	/* PG6-PG7 */
+#if IS_ENABLED(CONFIG_UART2_PORT_D)
+	{ "uart2",	5 },	/* PD1-PD2 */
+#else
 	{ "uart2",	7 },	/* PB0-PB1 */
+#endif
 	{ "uart3",	7 },	/* PB6-PB7 */
 	{ "pwm7",	5 },	/* PD22 */
 };

include/sunxi_gpio.h
@@ -120,6 +120,7 @@ enum sunxi_gpio_number {
 #define SUN8I_A83T_GPB_UART0	2
 #define SUN8I_V3S_GPB_UART0	3
 #define SUN8I_R528_GPB_UART2	7
+#define SUN8I_R528_GPD_UART2	5
 #define SUN50I_GPB_UART0	4
 
 #define SUNXI_GPC_NAND		2